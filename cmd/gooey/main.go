e7eb5a5f73644d63ed9fb5a7a1d0325b20f40dfd
andre <andrlzpt@protonmail.com>
2026-06-22 13:21:10 -0300

    :sparkles: Demo in main.go

--- cmd/gooey/main.go
+++ cmd/gooey/main.go
@@ -10,8 +10,10 @@ import (
 
 const WindowWidth = 640
 const WindowHeight = 480
-const CellWidth = 8
-const CellHeight = 12
+const CellWidth = 4
+const CellHeight = 4
+
+const EraseCircleRadius = 48
 
 var windowConfig = window.Config{
 	Width:  WindowWidth,
@@ -20,8 +22,10 @@ var windowConfig = window.Config{
 }
 
 var renderConfig = renderer.Config{
-	CellWidth:  CellWidth,
-	CellHeight: CellHeight,
+	WindowWidth:  WindowWidth,
+	WindowHeight: WindowHeight,
+	CellWidth:    CellWidth,
+	CellHeight:   CellHeight,
 }
 
 func main() {
@@ -37,6 +41,48 @@ func main() {
 
 func loop(state *window.State, buffer *ascii.Buffer) {
 	buffer.Clear()
-	buffer.FillRandom()
+	// buffer.FillRandom()
+	// ascii.DrawText(buffer, 2, 2, "GOOEY")
+
+	// if state.IsMouseInsideWindow {
+	// 	eraseCircle(buffer, state)
+	// }
+	glyph := ascii.Glyphs[len(ascii.Glyphs)-1]
+	ascii.DrawRect(buffer, 0, 0, 12, 12, glyph)
+
+	ascii.FillRect(buffer, 24, 0, 12, 12, glyph)
+
+	ascii.DrawCircle(buffer, 54, 8, 8, glyph)
+
+	ascii.FillCircle(buffer, 72, 8, 8, glyph)
+
 	renderer.Render(buffer, renderConfig)
+}
+
+func eraseCircle(buffer *ascii.Buffer, state *window.State) {
+	mouseX := int(state.MouseX)
+	mouseY := int(state.MouseY)
+
+	mouseCellX := mouseX / CellWidth
+	mouseCellY := mouseY / CellHeight
+
+	radius := EraseCircleRadius
+	radiusSquared := radius * radius
+
+	radiusCellsX := radius/CellWidth + 1
+	radiusCellsY := radius/CellHeight + 1
+
+	for y := mouseCellY - radiusCellsY; y <= mouseCellY+radiusCellsY; y++ {
+		for x := mouseCellX - radiusCellsX; x <= mouseCellX+radiusCellsX; x++ {
+			cellCenterX := x*CellWidth + CellWidth/2
+			cellCenterY := y*CellHeight + CellHeight/2
+
+			dx := cellCenterX - mouseX
+			dy := cellCenterY - mouseY
+
+			if dx*dx+dy*dy <= radiusSquared {
+				buffer.ClearAt(x, y)
+			}
+		}
+	}
 }
